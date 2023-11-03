9f3e57c4387d15cdec6c6efd4e255358dd35b6a5
Larissa <73025368+larissafazza@users.noreply.github.com>
2023-11-03 09:02:40 -0300

    develop users initial configuration

---
+++ findRooms/resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body text-center">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <a href=""><button type="button" class="btn btn-create-hotel">Adicione seu Hotel</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
